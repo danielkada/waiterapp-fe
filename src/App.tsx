78a4ba4aa656fcb1120958f0b99b20204918797a
Kadã <dgames201615@outlook.com>
2023-01-07 18:11:38 -0300

    adding global assets and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
 export function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello world!</h1>
+    </>
+
+  );
 }
